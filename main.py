96cf513e524de657877984cab4c7ca151c0b8219
Mulla Javed <168650100+MullaZee@users.noreply.github.com>
2025-05-06 23:26:41 +0530

    sgrvrgsg

--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-#=========================================================================
-# [AutoDelete - Telegram bot to delete messages after specific time]      
-# Copyright (C) 2022 Arunkumar Shibu                       
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#=========================================================================
-
 #!/usr/bin/env python3
 import asyncio
 import logging
@@ -23,7 +5,7 @@ import signal
 import sys
 from time import time
 from pyrogram import Client, filters, idle
-from pyrogram.errors import FloodWait, ConnectionError
+from pyrogram.errors import FloodWait
 from pyrogram.types import Message
 from utils.info import Config
 from utils.database import (
@@ -76,10 +58,9 @@ class AutoDeleteBot(Client):
         try:
             if self.is_connected:
                 await self.stop()
-        except ConnectionError:
-            logger.debug("Client was already disconnected")
         except Exception as e:
-            logger.warning(f"Error during stop: {e}")
+            if "already terminated" not in str(e):
+                logger.warning(f"Error during stop: {e}")
         finally:
             self._is_running = False
             self._stop_event.set()
